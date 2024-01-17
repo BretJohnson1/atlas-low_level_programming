bfd53667a330b2ea91b9e8f5bbe5b612fb5a708e
root <bretjohnson004@gmail.com>
2024-01-17 20:32:14 +0000

    puts .13

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - puts
  *
- *Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  main puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
