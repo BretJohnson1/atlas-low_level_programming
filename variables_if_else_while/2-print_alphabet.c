db759818f7c1fd3de52f81c2091784f4bd016d57
root <bretjohnson004@gmail.com>
2024-01-18 20:49:45 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - print the aplphabet
+ *
+ *Return: always return 0
+ */
+int main (void)
+{
+  char ch = 'a';
+    for (ch = 'a'; ch > 'z'; ch++)
+      putchar(ch);
+    return (0);
+}
