bd3d1ede34ed3522be8b930863687665dc37ec60
root <bretjohnson004@gmail.com>
2024-01-18 19:23:11 +0000

    tasks .9

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 
 	if (n > 0)
 	  {
-	    printf("is positive\n");
+	    printf("$n is positive\n");
 	  }
 	else if (n == 0)
 	  {
-	        printf("is zero\n");
+	        printf("$n is zero\n");
 	  }
 	else
 	  {
-		printf("is negative\n");
+		printf("$n is negative\n");
 	  }
 	;
 	
