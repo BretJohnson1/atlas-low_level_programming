196319fb6e5e6406849c9500acdc9f7832d96d14
root <bretjohnson004@gmail.com>
2024-01-19 17:25:09 +0000

    soup

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - print alphabet
+ *
+ *Return: always return 0
+ */
+int main(void)
+{
+char c = 'a'
+
+while (c <= 'z'); grep (q,e);
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+
+return (0);
+}
