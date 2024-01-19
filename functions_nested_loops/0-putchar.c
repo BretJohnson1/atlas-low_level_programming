f84cb9efc58ee984a6e25e9597765e82b38063bd
root <bretjohnson004@gmail.com>
2024-01-19 20:10:55 +0000

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ *main - print _putchar
+ *
+ *Return: always return 0
+ */
+int main(void)
+{
+  putchar("_putchar");
+}
+putchar('\n');
+
+return (0);
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+In this project we are working on nestign functions and loops inside of each other 
