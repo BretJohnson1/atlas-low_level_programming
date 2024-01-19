734bfaae729a5bac07404005d0b7cb8529970bda
root <bretjohnson004@gmail.com>
2024-01-19 19:03:01 +0000

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print numbers
+ *
+ *Return; always return 0
+ */
+int main(void)
+{
+for (int i = 0; i < 16; i++)
+{
+printf("%x", i);
+}
+printf("\n");
+
+return (0);
+}
