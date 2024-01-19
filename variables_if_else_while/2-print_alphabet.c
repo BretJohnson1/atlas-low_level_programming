b351fc58277aede5870c8980865acbbfba5c42e1
root <bretjohnson004@gmail.com>
2024-01-19 16:52:09 +0000

    alphabet .2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  *
  *Return: always return 0
  */
-int main (void)
+int main(void)
 {
-  char c = 'a';
-  while (c<= 'z')
-    {
-      putchar(c);
-      c++;
-    }
-  putchar('\n');
-    return (0);
+char c = 'a';
+while (c<= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
