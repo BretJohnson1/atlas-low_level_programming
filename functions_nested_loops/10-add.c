a40e6a0977f8784ea062f215d1b619a49354a43e
root <bretjohnson004@gmail.com>
2024-01-22 20:45:34 +0000

    add again

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -4,15 +4,7 @@
  *
  *Return: always return 0
  */
-int add(int, int)
+int add(int a, int b)
 {
-int a = 89
-int b = 9
-int sum = (a + b)
-
-for (a + b;;)
-{
-putchar(sum);
-}
-return (0);
+return (a + b);
 }
