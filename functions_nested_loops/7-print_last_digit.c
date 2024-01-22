b3e1edb9b87f8edf3355029946fe53239b33dace
root <bretjohnson004@gmail.com>
2024-01-22 18:46:42 +0000

    pld .6

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,9 @@
  */
 int print_last_digit(int n)
 {
-{
-_putchar(n / 10);
-
-return (n / 10);
-}
-return (0);
+  n = n % 10;
+  if (n < 0)
+    n = n * (-1);
+  _putchar(n + '0')
+    return (n);
 }
