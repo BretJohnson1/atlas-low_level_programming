efab60f22bbe187d8f33a28a679c960f55b51e38
root <bretjohnson004@gmail.com>
2024-01-22 16:27:27 +0000

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *int _islower(int c) - lowercase characters
+ *
+ *Return: always return 0
+ */
+int _islower(int c)
+{
+  int c = 'a';
+
+  if (c = 'a');
+    {
+      return (1);
+      }
+  else if (c = 'A');
+    {
+      return (0);
+    }
+}
