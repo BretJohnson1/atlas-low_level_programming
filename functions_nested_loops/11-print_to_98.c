d9640c27bc04f6ff38dfaec6d688d7452552529e
root <bretjohnson004@gmail.com>
2024-01-22 20:59:56 +0000

    OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - prints numbers
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+void print_to_98(int n)
+{
+n = 0
+
+for (n = 0; n<= 98; n++)
+{
+printf(("%d, \n"(n))
+}
+}
