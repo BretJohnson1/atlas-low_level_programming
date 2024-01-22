a2c379aa1129694f9f018b90f19151219952c63e
root <bretjohnson004@gmail.com>
2024-01-22 19:13:30 +0000

    24

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *jack_bauer - print every minute
+ *
+ *Return: always return 0
+ */
+void jack_bauer(void)
+{
+int m = 1;
+int h = 0;
+
+while (h >= 0 && h < 13)
+{
+_putchar(h);
+
+while (i >= 0 && i <=59)
+
+if (i >59)
+h++;
+}
+return (0);
+}
