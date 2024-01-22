554fafe51b4adead6e3dd068cd2938906843e696
root <bretjohnson004@gmail.com>
2024-01-22 18:16:05 +0000

    pld

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit - print last digit of number
+ *@n: checked output
+ *Return: last digit of number
+ */
+int print_last_digit(int n)
+{
+_putchar(n % 10)
+
+return (n % 10)
+}
