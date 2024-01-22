872c625a5f40aa21fff641eddd291f9689eeee54
root <bretjohnson004@gmail.com>
2024-01-22 20:42:15 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *add - adds integers
+ *
+ *Return: always return 0
+ */
+int add(int a, int b)
+{
+int a = 89
+int b = 9
+int sum = (a + b)
+
+for (a + b;;)
+{
+putchar(sum);
+}
+return (0);
+}
