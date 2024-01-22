7215a68c1d2dd5c05db2bc5e51229c532553063f
root <bretjohnson004@gmail.com>
2024-01-22 17:56:16 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_abs - compute absolute values
+ *
+ *Return: always return 0
+ */
+int _abs(int)
+{
+int r;
+
+if (r < 0)
+{
+return (-r);
+}
+else
+{
+return (r);
+}
+reutrn (0);
+}
