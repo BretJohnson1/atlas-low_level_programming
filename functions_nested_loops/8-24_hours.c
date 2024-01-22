776c1df41af6ffc7046d29b63e5ad7c46100d5a2
root <bretjohnson004@gmail.com>
2024-01-22 19:49:02 +0000

    24 .11

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,14 +9,14 @@ void jack_bauer(void)
 int h = 0-23;
 int m = 0-59;
 
-for (h = 0; h <= 24; h++)
+for (h = 0; h <= 24; h+=)
+for (m = 0; m <=59; m++)
 {
-_putchar(h);
-h++;
-}
-for (m = 0; m <=59; m++);
-{
-putchar(m);
-h++;
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
+_putchar(':')
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+_putchar('\n')
 }
 }
