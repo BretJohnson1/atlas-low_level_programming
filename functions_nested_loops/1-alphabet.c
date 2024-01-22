a1417e76b9f828dded119d930c25e706ee2b9dd7
root <bretjohnson004@gmail.com>
2024-01-22 15:20:57 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - print alphabet
+ *
+ *Return: always return 0
+ */
+int main(void)
+{
+  char c = 'a'
+
+    while (c <= 'z')
+      {
+	_putchar(c);
+	c++;
+      }
+  putchar('\n');
+
+    return (0);
+}
