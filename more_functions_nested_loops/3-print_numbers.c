fa0d11d411da05d8716a4ae5264963f877799a3e
root <bretjohnson004@gmail.com>
2024-01-24 17:13:12 +0000

    numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_numbers - print numbers
+ *
+ *Return: always return 0
+ */
+void print_numbers(void)
+{
+int i;
+
+if (i <= 9 && >= 0)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n')
+
+return (0);
+}
