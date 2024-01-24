715a49dac2835418901ea6c1427c2ac514e73941
root <bretjohnson004@gmail.com>
2024-01-24 16:44:00 +0000

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isdigit - check for digit
+ *@c: checked output
+ *
+ *Return: return 1 or 0
+ */
+int _isdigit(int c)
+{
+if (c <= 9 && c >= -1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
