b44ba375ce1617fca253cbd9cb34159e5711bd07
root <bretjohnson004@gmail.com>
2024-01-24 18:49:11 +0000

    symbols

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
   for (i = 0; i <= 14; i++)
     {
       _putchar((i % 10) + 48);
-      for (n = 10; n <= 14; n++)
+      for (n >= 10; n <= 14; n++)
 	{
-	  if (i > 10)
+	  if (i >= 10)
 	    {
 	      _putchar((i / 10) + 48);
 	    }
