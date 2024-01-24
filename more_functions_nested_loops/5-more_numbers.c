68f7f8e9f79e78f402eac59e9cdb6aae5bb02e57
root <bretjohnson004@gmail.com>
2024-01-24 18:22:59 +0000

    language

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers - print numbers
+ *
+ *Return: always return 0
+ */
+void more_numbers(void)
+{
+  int i = 0;
+  int n = 0;
+
+  for (i = 0; i < 10; i++)
+    {
+      for (n = 0; n <= i; n++)
+	{
+      _putchar(48 + i);
+	}
+    }
+  putchar('\n');
+  i++;
+}
