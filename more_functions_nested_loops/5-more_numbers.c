2ff58911031e8c9fee2ea365813a4d2290cee7bf
root <bretjohnson004@gmail.com>
2024-01-24 18:53:33 +0000

    language

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@ void more_numbers(void)
   int i = 0;
   int n = 1;
 
-  for (i = 0; i <= 14; i++)
+  for (i = 1; i <= 10; i++)
     {
-      _putchar((i % 10) + 48);
-      for (n >= 10; n <= 14; n++)
+      for (n = 0; n <= 14; n++)
 	{
 	  if (i >= 10)
 	    {
 	      _putchar((i / 10) + 48);
 	    }
+	  _putchar((i % 10) + 48);
 	}
     }
   _putchar('\n');
