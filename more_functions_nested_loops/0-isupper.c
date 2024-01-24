ceaa5c9bcf63c9ded691525bfd53e1c442677a0d
root <bretjohnson004@gmail.com>
2024-01-24 16:20:53 +0000

    upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *isupper- check uppercase
+ *@c: checked output
+ *
+ *Return: always return 0
+ */
+int _isupper(int c)
+{
+  if (c = 'A')
+    {
+      return (1);
+    }
+  else()
+	{
+	  return (0);
+	}
+  return (0);
+}
