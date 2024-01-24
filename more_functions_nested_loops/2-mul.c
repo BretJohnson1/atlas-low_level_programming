eeb5b86fd06e54bac14edd7dfc5c0a3cdf5f6d77
root <bretjohnson004@gmail.com>
2024-01-24 17:00:33 +0000

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiply
+ *@a: checked output
+ *@b: checked output
+ *
+ *Return: always return 0
+ */
+int mul(int a, int b)
+{
+mul(int a, int b)
+
+return (0);
+}
