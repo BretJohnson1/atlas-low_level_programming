a92706a68b7cc6c14f256621d54f8b296d0d20b3
root <bretjohnson004@gmail.com>
2024-01-24 19:19:05 +0000

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_line - print a line
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+void print_line(int n)
+{
+  while (n > 0)
+    {
+      _putchar("_ \n");
+      if (n <= 0)
+	{
+	  _putchar('\n');
+	}
+    }
+}
