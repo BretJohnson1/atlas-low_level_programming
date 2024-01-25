1a489dd6373ef935610bfffd007690688ea8b9a1
root <bretjohnson004@gmail.com>
2024-01-25 16:24:41 +0000

    parallel revised

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,12 +7,17 @@
  */
 void print_diagonal(int n)
 {
-  int a;
+int a;
+int b;
 
-  if (n > 0)
-    for (a = 0; a < n; a++)
-      {
-	_putchar('\\');
-	  }
-  _putchar('\n');
-    }
+if (n > 0)
+for (a = 0; a < n; a++)
+{
+for (b = 0; b <= a; b++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+}
+_putchar('\n');
+}
