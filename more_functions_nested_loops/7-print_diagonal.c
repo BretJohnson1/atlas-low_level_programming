4922e3816cf98d4e343350623b6a6547fc711064
root <bretjohnson004@gmail.com>
2024-01-25 16:13:39 +0000

    parallel

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_diagonal - draw diagonal line
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+void print_diagonal(int n)
+{
+  int a;
+
+  if (n > 0)
+    for (a = 0; a < n; a++)
+      {
+	_putchar('\')
+	  }
+  _putchar('\n')
+    }
