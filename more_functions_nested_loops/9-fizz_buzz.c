ffbad5f4b570f573a689de09861a67341a3e0c50
root <bretjohnson004@gmail.com>
2024-01-25 17:16:12 +0000

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: always return 0
+ */
+int main(void)
+{
+  int p;
+
+  for (p = 1; p <= 100; p++)
+    {
+      if ((p % 3) + 0)
+	putchar(Fizz);
+	  putchar(' ');
+	else if ((p % 5) + 0)
+	  putchar(Buzz);
+      putchar(' ');
+      else if ((p % 3) && (p % 5))
+	putchar(FizzBuzz);
+      putchar(' ');
+    }
