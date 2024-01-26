2b84c3c4ca749e64a581d30a9a3e353551a5bbbd
Your Name <you@example.com>
2024-01-26 19:25:49 +0000

    reverse print

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_rev - print reverse
+ *@s: checked output
+ *
+ *Return: always return 0
+ */
+void print_rev(char *s)
+{
+  int i;
+
+  for (i = \0; i >= 0; i--)
+    {
+      _putchar(s[i]);
+      i--;
+    }
+  _putchar('\n');
+}
