09ef315216c7284c084d9b83c59348ca78ff6fe7
Your Name <you@example.com>
2024-01-29 18:53:12 +0000

    leaders

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *_atoi - convert a string to an integer
+ *@s: checked output
+ *
+ *Return: always return 0
+ */
+int _atoi(char *s)
+{
+  int num;
+  int sign;
+
+  if (*s == '+' || *s == '-')
+    {
+      sign = (*s == '-')
+	? -1 : 1;
+      s++;
+    }
+  while (*s != '\0')
+    {
+      if (*s >= '0' && *s <= '9')
+	{
+	  num = num * 10 + (*s - '0');
+	}
+      else
+	{
+	  return (0);
+	}
+      *s++;
+    }
+  return (sign * num);
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *@dest: checked output
+ *@src: checked output
+ *
+ *Return: always return 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+  
