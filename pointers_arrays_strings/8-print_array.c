7de2166a1c4f4c98f69cbc4bd7eb272bbee870a8
Your Name <you@example.com>
2024-01-29 18:04:47 +0000

    eight

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_array - print array of integers
+ *@a: checked output
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+void print_array(int *a, int n)
+{
+  int len = 0;
+  
+  while (a != '\0')
+    {
+      printf("%s = ",  a)
+	a++;
+	}
+  while (n <= len)
+    {
+      printf("%d");
+    }
+  -putchar('\n');
+}
