4a2bebb6affc135d043fe635e0d92d245c0e26f5
Your Name <you@example.com>
2024-01-29 17:02:22 +0000

    seven

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *puts_half - print second half of string
+ *@str: checked output
+ *
+ *Return: always return 0
+ */
+void puts_half(char *str)
+{
+  int len = 0;
+  n = (len - 1 / 2)
+
+    while ( len != '\0')
+      {
+	len++;
+	for (n = len - 1 / 2)
+	  {
+	    _putchar(*str);
+	      }
+      }
+  _putchar('\n');
