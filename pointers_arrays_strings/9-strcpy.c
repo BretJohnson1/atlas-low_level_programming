f4e9e7392c20c276a7503e468d09e036611cf8f8
Your Name <you@example.com>
2024-01-30 00:51:44 +0000

    strcopy revised

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ *_strcpy - copy a string
  *@dest: checked output
  *@src: checked output
  *
@@ -8,12 +9,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (dest);
+return (dest);
 }
   
