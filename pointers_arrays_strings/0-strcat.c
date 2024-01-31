6183c610831ba312900721d322e1e52eb34442c1
Your Name <you@example.com>
2024-01-31 16:52:43 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strcat - concatenate two strings
+ *@dest: checked output
+ *@src: checked output
+ *
+ *Return: always return 0
+ */
+char *_strcat(char *dest, char *src)
+{
+  int i = 0;
+  
+  for (i = 0; i <= '\0'; i++)
+    {
+      if (dest[i] = '\0')
+	src++;
+    }
+  dest = '\0';
+  return (0);
+}
+  
