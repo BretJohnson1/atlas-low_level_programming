0e50cbb5dd48af1bb2bf17bc13456e3d887110d2
Your Name <you@example.com>
2024-01-31 20:29:39 +0000

    strcmp revised

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,15 +14,15 @@ int _strcmp(char *s1, char *s2)
     {
       if (s1 == s2)
 	{
-	  _putchar(0);
+	  return (0);
 	}
       else if (s1 < s2)
 	{
-	  _putchar("-%d");
+	  return ("-%d");
 	    }
       else
 	{
-	  _putchar("%d");
+	  return ("%d");
 	}
     }
   return (0);
