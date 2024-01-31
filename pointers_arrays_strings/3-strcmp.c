9c464c2de9804cd75e528b3c22ec1e5af487aaa3
Your Name <you@example.com>
2024-01-31 20:48:47 +0000

    strcmp .3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,15 @@
 int _strcmp(char *s1, char *s2)
 {
   int i = 0;
+  int a = 0;
 
-  for (i = 0; i < '\0'; i++)
+  for (i = 0; i != '\0'; i++)
     {
-      if (s1 == s2)
+      if (s1[i] == s2[a])
 	{
 	  return (0);
 	}
-      else if (s1 < s2)
+      else if (s1[i] < s2[a])
 	{
 	  return (-1);
 	    }
