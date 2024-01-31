eb58c056a64779088328fea2bc921d53072230dc
Your Name <you@example.com>
2024-01-31 19:26:33 +0000

    strncpy .14

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
   int i = 0;
+  int a = 0;
 
   while (src[i] != '\0')
     {
-      dest[i++] = src[i++];
+      if (a < n)
+      dest[i++] = src[a++];
     }
   dest[i++] = '\0';
   return (dest);
