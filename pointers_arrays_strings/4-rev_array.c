301fa363ffe0a96cef20a8a630e6c2caaee17811
Your Name <you@example.com>
2024-02-01 17:54:18 +0000

    rev

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *reverse_array - reverse content
+ *@a: checked output
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int m;
+
+	for (i = 0; i <= n; i++)
+	{
+		return (a[i]);
+
+		for (m = n; m >= 0; m--)
+		{
+			return (a[i]);
+		}
+	}
+}
