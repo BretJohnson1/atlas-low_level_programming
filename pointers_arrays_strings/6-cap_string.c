6839eb30d7e5631455bd6063dcf5967574f8c7f1
Your Name <you@example.com>
2024-02-02 18:25:35 +0000

    string .6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,8 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
+		if (a[i] > 'a' && a[i] < 'z')
+		{
 		switch (a[i])
 			{
 			case ' ':
@@ -28,6 +30,7 @@ char *cap_string(char *a)
 			case '}':
 				a[i + 1] = a[i + 1] -32;
 					}
+		}
 	}
 	return (a);
 }
