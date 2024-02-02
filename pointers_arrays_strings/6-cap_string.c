adf66ff7f5fb39fb8e1689ef04286e36f4a34607
Your Name <you@example.com>
2024-02-02 17:59:50 +0000

    string .2

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,19 +13,19 @@ char *cap_string(char *a)
 	{
 		switch (a[i])
 			{
-			case 1: " ";
-			case 2: "\t";
-			case 3: "\n";
-			case 4: ",";
-			case 5: ";";
-			case 6: ".";
-			case 7: "!";
-			case 8: "?";
+			case 1: ' ';
+			case 2: '\t';
+			case 3: '\n';
+			case 4: ',';
+			case 5: ';';
+			case 6: '.';
+			case 7: '!';
+			case 8: '?';
 			case 9: '"';
-			case 10: "(";
-			case 11: ")";
-			case 12: "{";
-			case 13: "}";
+			case 10: '(';
+			case 11: ')';
+			case 12: '{';
+			case 13: '}';
 					a[i + 1] = a[i + 1] -32
 					}
 	   return (a);
