5cb9835a0d4da5761762ee2a7d7b421f5a6dea23
Your Name <you@example.com>
2024-02-05 16:35:14 +0000

    leet revised

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,26 +7,21 @@
  */
 char *leet(char *a)
 {
-	int i, j;
-    char leet[256] = {0};
-	
-    leet['a'] = leet['A'] = '4';
-    leet['e'] = leet['E'] = '3';
-    leet['o'] = leet['O'] = '0';
-    leet['t'] = leet['T'] = '7';
-    leet['l'] = leet['L'] = '1';
-	
-    for (i = 0, j = 0; a[i]; i++)
-    {
-        if (leet a[i])
-        {
-            a[j++] = a[i];
-        }
-        else
-        {
-            a[j++] = a[i];
-        }
-    }
-    a[j] = '\0';
+	int i;
+	int b;
+
+	char letter[] = "aAeEoOtTlL"
+	char number[] = "4433007711"
+
+		for (i = 0; letter[i] != '\0'; i++)
+		{
+			for (b = 0; number[b] != "10"; b++)
+			{
+				if (letter[a] == letter[i])
+				{
+					letter[a] == number[b];
+				}
+			}
+		}
     return (a);
 }
