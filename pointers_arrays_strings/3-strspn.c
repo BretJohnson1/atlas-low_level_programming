dee7d24ba8dbf999e6c9ba375b03ce8f1a86ab93
Your Name <you@example.com>
2024-02-05 22:36:16 +0000

    finally

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,25 +8,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int result = 0;
-	int j;
+	unsigned int i, j, count = 0;
 
-	while(*s)
+	for (i = 0; s[i] != '\0' && s[i] != ' ' && s[i] != ','; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-		if (*s == accept[j])
-		{
-			result++;
-		break;
-		}
-		else if (accept[j+1] == '\0')
-		{
-			return (result);
-		s++;
-		}
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
 		}
 	}
-		
-	return (result);
+	return (count);
 }
