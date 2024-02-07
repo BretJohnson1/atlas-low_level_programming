810e6a68cb4385d4e5281fdbc6a3f55ab2991197
Your Name <you@example.com>
2024-02-07 17:02:42 +0000

    puts

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_puts_recursion - print a string
+ *@s: checked output
+ *
+ *Return: always return 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
---
+++ recursion/main.h
@@ -0,0 +1,4 @@
+#ifndef
+#define
+void _puts_recursion(char *s);
+#endif
