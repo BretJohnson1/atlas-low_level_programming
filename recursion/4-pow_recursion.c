8a7aef79bb91168bfbff7c4c304937a8c5d18dab
Your Name <you@example.com>
2024-02-07 20:13:33 +0000

    power

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_pow_recursion - return x to power of y
+ *@x: checked output
+ *@y: checked output
+ *
+ *Return: always return 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return _pow_recursion(x^y);
+}
