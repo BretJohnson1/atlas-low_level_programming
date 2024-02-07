1658cdcd76a5106c7cb9361909cf9d0b6522a795
Your Name <you@example.com>
2024-02-07 19:24:33 +0000

    fact

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *factorial - factorial of a number
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	n = n!
+		factorial(n);
+}
