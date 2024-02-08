d4c47852c3678e01928274b74793db327f2bc12c
Your Name <you@example.com>
2024-02-08 16:22:31 +0000

    square

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - return natural square root
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (_sqrt_recursion(n/n) / 2);
+}
