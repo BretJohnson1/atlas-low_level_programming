e77ae358493197d460501497a2c519bba6ff4ede
Your Name <you@example.com>
2024-02-09 19:35:49 +0000

    infinity

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main - add positive numbers
+ *@argc: checked output
+ *@argv: checked output
+ *
+ *Return: always return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int a = argv[i];
+	int result;
+
+	for (i = 0; i < strlen(a); i++)
+		{
+			if (a[i] < 48 || a[i] > 57)
+			{
+				printf("%d\n", result)
+					}
+			result += atoi(a)
+				a++
+
+	if (argc == 0)
+	{
+		printf("0\n");
+	}
+			return (0);
