57f027b9b635bccd454d69c1d5f0e164268855ce
Your Name <you@example.com>
2024-02-09 18:12:54 +0000

    mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - multiply two numbers
+ *@argc: checked output
+ *@argv: checked output
+ *
+ *Return: always return 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = 2, b = 5, result = (a * b);
+	(void) argc;
+	(void) argv;
+	
+	if (result < 2)
+	{
+		printf("Error\n")
+			return (1);
+	}
+	else if (result >= 2)
+	{
+		printf("%d", result);
+		return(result);
+	}
+	return (0);
+}
