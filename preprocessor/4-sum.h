2668e26b82a72815e0bee0e0cbdad2a3096023ae
Your Name <you@example.com>
2024-02-19 16:35:32 +0000

    task 4

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define SUM(x, y) ((x) + (y))
+/**
+ *main - compute sums
+ *
+ *Return: always return 0
+ */
+int main(void)
+{
+	int a = 5;
+	int b = 3;
+	int result = SUM(a, b);
+
+	printf("Sum of %d and %d is %d\n", a, b, result);
+	return (0);
+}
