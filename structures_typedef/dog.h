567e400f0356fd1648bf1f5dcc6e981d833b34d8
Your Name <you@example.com>
2024-02-21 16:03:17 +0000

    task 0 function

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *struct - data structure
+ *main - data structure
  *
  */
-
+int main(void)
+{
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
+}
 }
