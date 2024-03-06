2e7e246d649d5269f671fb86d19e6859dfefa4d0
Your Name <you@example.com>
2024-03-06 18:33:08 +0000

    used the wrong function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,9 +8,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
 	if (str == NULL)
 		return (NULL);
+
+	while (str[len] 1= '\0')
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -23,7 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-new_node->len = strlen(str);
+new_node->len = len;
 new_node->next = *head;
 *head = new_node;
 
