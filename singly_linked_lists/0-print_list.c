e5925c0cb168909caa94413febb42f228cd8773d
Your Name <you@example.com>
2024-03-06 17:59:37 +0000

    stuck for real

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,21 +3,6 @@
 #include <stddef.h>
 #include "lists.h"
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-
-/**
  *print_list: print all elements of a list
  *@h - checked output
  *
