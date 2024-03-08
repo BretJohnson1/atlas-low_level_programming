4a4090647af40fc3c6d061572dec043a1764d9f4
Your Name <you@example.com>
2024-03-08 17:23:23 +0000

    not sure whats happening

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
-		return (add_nodeint_at_index(head, n));
+		return (add_nodeint(head, n));
 
 				temp = *head;
 				for (i = 0; temp != NULL && i < idx - 1; i++)
