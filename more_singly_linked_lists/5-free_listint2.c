7d2f3923d2f9c6ab355d8507f57929374d8cec8a
Your Name <you@example.com>
2024-03-08 16:42:09 +0000

    freeing more lists

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_listint2 - frees list and sets to null
+ *@head: checked output
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
