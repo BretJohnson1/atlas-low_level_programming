9da9c73b90f4b7a9b4b7f0c72c35bf5b87e535f8
Your Name <you@example.com>
2024-03-08 18:08:42 +0000

    stuck

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -7,6 +7,31 @@
  *Return: always return 0
  */
 listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *new_node, *last;
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = n;
+    new_node->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        last = *head;
+        while (last->next != NULL)
+        {
+            last = last->next;
+        }
+        last->next = new_node;
+    }
+    return (new_node);
+}
 /**
  *insert_nodeint_at_index - inserts node at any position
  *@head: checked output
