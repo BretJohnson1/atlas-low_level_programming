a4c9c5205102e5fc84d79e33b0ec10bcab9b4b41
Your Name <you@example.com>
2024-03-11 15:32:38 +0000

    perhaps a small fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -37,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer[bytes_read] = '\0';
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || (size_t)bytes_written != bytes_read)
+	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
 	{
 		free(buffer);
 		close(fd);
