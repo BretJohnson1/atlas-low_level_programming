65308999ed242eb0c3d6ef0700f0b5752ec2dbae
Bret Johnson <you@example.com>
2024-03-13 17:01:15 +0000

    syntactical shenanigans

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  *
  *Return: always return 0
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head. unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
