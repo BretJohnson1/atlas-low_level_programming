5f041e2a57a2bc3fc098753565e4d5c02c6b1745
Bret Johnson <you@example.com>
2024-03-15 17:16:52 +0000

    its as simple as that

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	new_node->key = strdup(key);
-	if(new_node->key == NULL)
+	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
