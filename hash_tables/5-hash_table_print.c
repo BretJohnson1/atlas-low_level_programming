4b15bd2fc11f7a5b1cd75bf089d75dced34ad380
Bret Johnson <you@example.com>
2024-03-15 17:31:58 +0000

    typo

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 			if (!first)
 				printf(", ");
 			printf("'%s': '%s'", node->key, node->value);
-			frist = 0;
+			first = 0;
 			node = node->next;
 		}
 	}
