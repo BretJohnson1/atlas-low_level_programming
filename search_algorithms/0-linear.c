09d08dfb110183ca950504f307b75cbe82a084b4
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-04-10 09:37:09 -0500

    Minor syntax fix in 0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,12 +7,12 @@
 */
 int linear_serach(int *array, size_t size, int value)
 {
+    size_t i;
+    
     if (array == NULL)
     {
         return (-1);
     }
-
-    size_t i;
 
     for (i = 0; i < size; i++)
     {
