a0f6c75fb77cc26817d3a2f59ce62fd89bfc1c99
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-04-10 09:29:16 -0500

    Created 0-linear.c and search_algos.h

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+* linear_search - search algorithm
+*@array: pointer to the head of thea array
+*@size: size of the array
+*@value: value being searched
+*/
+int linear_serach(int *array, size_t size, int value)
+{
+    if (array == NULL)
+    {
+        return (-1);
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Comparing %d\n", array[i]);
+        if (array[i] == value)
+        {
+            return (i);
+        }
+    }
+    return (-1);
+}
