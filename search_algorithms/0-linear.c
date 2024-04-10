09ec16921f6db21bfb80b22bfd56f3f331d105ea
BretJohnson1 <bretjohnson004@gmail.com>
2024-04-10 17:03:28 +0000

    final touches on 0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,8 +1,12 @@
 #include "search_algos.h"
-
+/**
+ *linear_search - search algorithm
+ *@array: pointer to the head of the array
+ *@Size: size of the array
+ *@value: value being checked
+ */
 int linear_search(int *array, size_t size, int value)
 {
-
 	size_t i;
 
 	if (array == NULL)
@@ -14,9 +18,9 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			return (i);
-		}
+			{
+				return (i);
+			}
 	}
-	return (-1);
+		return (-1);
 }
