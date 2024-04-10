952cba0060c99300e7d6bec8a1d0ff1e26b99922
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-04-10 09:55:58 -0500

    removing documentation from 0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,10 +1,5 @@
 #include "search_algos.h"
-/**
-* linear_search - search algorithm
-*@array: pointer to the head of thea array
-*@size: size of the array
-*@value: value being searched
-*/
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
