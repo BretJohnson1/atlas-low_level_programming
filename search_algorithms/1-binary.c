12d11e5ea50afd48d75c74e9ca9f4f72e159c195
BretJohnson1 <bretjohnson004@gmail.com>
2024-04-10 17:32:17 +0000

    Fixing Kernel style coding

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,31 +10,31 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0, right = size - 1, mid, i;
+	size_t left = 0, right = size - 1, mid, i;
 
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+		return -1;
 
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
 
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i != left)
-                printf(", ");
-            printf("%d", array[i]);
-        }
-        printf("\n");
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i != left)
+				printf(", ");
+			printf("%d", array[i]);
+		}
+		printf("\n");
 
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
