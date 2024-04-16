a6774d8ee2b37407ffc747e6120def81e6a9d422
BretJohnson1 <bretjohnson004@gmail.com>
2024-04-16 14:27:42 +0000

    Finalized 0-linear.c

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,8 +1,5 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-#include <stddef.h>
-#include <stdio.h>
+#ifndef search_algos
+#define search_algos
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
